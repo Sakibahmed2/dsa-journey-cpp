4ee47cfc5603ad6280767489068e5338e129d7dc
shakib-chahealth <mauijaloskor@gmail.com>
2026-07-01 15:43:40 +0600

    insert at any position in linked list

--- basic-data-structure/m6-singly-linked-list-operations/insert_at_any_position.cpp
+++ basic-data-structure/m6-singly-linked-list-operations/insert_at_any_position.cpp
@@ -16,13 +16,26 @@ public:
 
 void print_linked_list(Node *head)
 {
-   
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->value << endl;
+        temp = temp->next;
+    }
 }
 
-void insert_at_any_position(Node *&head,int idx, int val)
+void insert_at_any_position(Node *&head, int idx, int val)
 {
-    Node* newNode = new Node(val);
-    
+    Node *newNode = new Node(val);
+    Node *temp = head;
+
+    for (int i = 1; i < idx; i++)
+    {
+        temp = temp->next;
+    }
+
+    newNode->next = temp->next;
+    temp->next = newNode;
 }
 
 int main()
@@ -34,8 +47,7 @@ int main()
     head->next = a;
     a->next = b;
 
-   
-
+    insert_at_any_position(head, 2, 100);
     print_linked_list(head);
 
     return 0;
