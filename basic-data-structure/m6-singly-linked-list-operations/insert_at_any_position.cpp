e83742f1014d04420580bb953650b5e378375394
shakib-chahealth <mauijaloskor@gmail.com>
2026-07-01 14:44:34 +0600

    add git ignore

---
+++ basic-data-structure/m6-singly-linked-list-operations/insert_at_any_position.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int value;
+    Node *next;
+
+    Node(int value)
+    {
+        this->value = value;
+        this->next = NULL;
+    }
+};
+
+void print_linked_list(Node *head)
+{
+   
+}
+
+void insert_at_any_position(Node *&head,int idx, int val)
+{
+    Node* newNode = new Node(val);
+    
+}
+
+int main()
+{
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+
+    head->next = a;
+    a->next = b;
+
+   
+
+    print_linked_list(head);
+
+    return 0;
+}
