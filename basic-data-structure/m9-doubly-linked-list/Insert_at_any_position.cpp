417e11c0009156ee974d234ed18f7ac6c89ced59
shakib-chahealth <mauijaloskor@gmail.com>
2026-07-15 12:25:48 +0600

    insert operations

---
+++ basic-data-structure/m9-doubly-linked-list/Insert_at_any_position.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int value;
+    Node *next;
+    Node *prev;
+
+    Node(int value)
+    {
+        this->value = value;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+void print_forward(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->value << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+};
+
+void insert_at_any_position(Node *&head, int idx, int val)
+{
+    Node *temp = head;
+    Node *newnode = new Node(val);
+    for (int i = 1; i < idx; i++)
+    {
+        temp = temp->next;
+    }
+
+    newnode->next = temp->next;
+    temp->next->prev = newnode;
+    temp->next = newnode;
+    newnode->prev = temp;
+}
+
+int main()
+{
+
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *tail = new Node(30);
+
+    head->next = a;
+    a->prev = head;
+    a->next = tail;
+    tail->prev = a;
+
+    insert_at_any_position(head, 2, 200);
+
+    print_forward(head);
+
+    return 0;
+}
