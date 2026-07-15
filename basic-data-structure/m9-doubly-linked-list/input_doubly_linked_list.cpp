1abc353594fb402335f245be8800790d30d0b2a6
shakib-chahealth <mauijaloskor@gmail.com>
2026-07-15 14:41:24 +0600

    input at doubly linked list

---
+++ basic-data-structure/m9-doubly-linked-list/input_doubly_linked_list.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int value;
+    Node *next;
+    Node *prev;
+
+    Node(int value)
+    {
+        this->value = value;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+void print_forward(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->value << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+};
+
+void insert_at_tail(Node *&head, Node *&tail, int val)
+{
+    Node *newnode = new Node(val);
+    if (head == NULL)
+    {
+        head = newnode;
+        tail = newnode;
+        return;
+    }
+    newnode->prev = tail;
+    tail->next = newnode;
+    tail = newnode;
+}
+
+int main()
+{
+
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    int val;
+    while (true)
+    {
+        cin >> val;
+        if (val == -1)
+        {
+            break;
+        }
+        insert_at_tail(head, tail, val);
+    }
+
+    print_forward(head);
+
+    return 0;
+}
