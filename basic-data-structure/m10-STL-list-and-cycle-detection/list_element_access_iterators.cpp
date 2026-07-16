55136e913da63ce94d32ee87260d86d800d0a8ff
shakib-chahealth <mauijaloskor@gmail.com>
2026-07-16 15:57:46 +0600

    stl all operations done

---
+++ basic-data-structure/m10-STL-list-and-cycle-detection/list_element_access_iterators.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    list<int> l = {1, 2, 3, 4, 5};
+
+    // cout << l.back() << endl; // access the tail element
+    
+    // cout << l.front() << endl; // access the head element
+    // cout  << *l.begin() << endl;
+
+    // cout << *next(l.begin(), 2) << endl; // access the ith element
+
+
+
+    return 0;
+}
