931f63cf4325c7c4c19d569681298c952bfea9de
shakib-chahealth <mauijaloskor@gmail.com>
2026-08-19 21:31:42 +0600

    Adjacency Matrix undirected and directed graph input

--- basic-data-structure/problemeset-part-2/Maximise_the_Sum.cpp
+++ basic-data-structure/problemeset-part-2/Maximise_the_Sum.cpp
@@ -4,6 +4,35 @@ using namespace std;
 int main()
 {
     int t;
-    cin >>  
+    cin >> t;
+    while (t--)
+    {
+        vector<int> v(5);
+        for (int &x : v)
+        {
+            cin >> x;
+        }
+
+        int mx = 0;
+        for (int i = 1; i < 5; i++)
+        {
+            if (v[mx] > v[i])
+            {
+                v[i] = v[i] * -1;
+            }
+            else
+            {
+                v[mx] = v[mx] * -1;
+                mx = v[i];
+            }
+        }
+
+        for (int x : v)
+        {
+            cout << x << " ";
+        }
+        cout << endl;
+    }
+
     return 0;
 }
