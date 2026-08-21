c7ecc4a93032698c5f76a7c95ebbc93475864892
shakib-chahealth <mauijaloskor@gmail.com>
2026-08-21 20:22:11 +0600

    Single Source Shortest Distance using BFS

---
+++ introductio-to-algorithms/m2-BFS/single_source_shortest_distance.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> adj_list[1005];
+bool visited[1005];
+int level[1005];
+
+void bfs(int src)
+{
+    queue<int> q;
+    q.push(src);
+    visited[src] = true;
+    level[src] = 0;
+
+    while (!q.empty())
+    {
+        int parent = q.front();
+        q.pop();
+
+        for (int child : adj_list[parent])
+        {
+            if (!visited[child])
+            {
+                q.push(child);
+                visited[child] = true;
+                level[child] = level[parent] + 1;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+    while (e--)
+    {
+        int a, b;
+        cin >> a >> b;
+        adj_list[a].push_back(b);
+        adj_list[b].push_back(a);
+    }
+
+    int src, dest;
+    cin >> src >> dest;
+
+    memset(visited, false, sizeof(visited));
+    memset(level, -1, sizeof(level));
+
+    bfs(src);
+
+    cout << level[dest] << endl;
+
+    return 0;
+}
